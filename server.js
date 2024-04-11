f2cd69bceb914219f1635289a7c09907af8e1025
Mikey Haynes <nightowl898@gmail.com>
2024-04-11 09:04:47 -0700

    finished node api version 2

--- server.js
+++ server.js
@@ -1,11 +1,48 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
+const mongoose = require('mongoose')
+const productRoute = require('./routes/productRoute');
+const errorMiddleware = require('./middleware/errorMiddleware')
+const cors = require('cors')
 
-// routes 
-app.get('/', (req, res)=> {
-    res.send('Hello Node API')
+const app = express()
+ 
+const PORT = process.env.PORT || 3000
+const MONGO_URL = process.env.MONGO_URL
+const FRONTEND = process.env.FRONTEND
+
+var corsOptions = {
+    origin: FRONTEND,
+    optionsSuccessStatus: 200 // some legacey browsers(IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+//routes
+
+app.use('/api/products', productRoute);
+
+app.get('/', (req, res) => {
+  
+    res.send('Hello NODE API')
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API app is running on port 3000')
+app.get('/blog', (req, res) => {
+    res.send('Hello Blog, My name is Devtamin')
+})
+
+app.use(errorMiddleware)
+
+mongoose.set("strictQuery", false)
+mongoose.
+connect(MONGO_URL)
+.then(() => {
+    console.log('connected to MongoDB')
+    app.listen(PORT, ()=> {
+        console.log(`Node API app is running on port ${PORT}`)
+    });
+}).catch((error) => {
+    console.log(error)
 })
